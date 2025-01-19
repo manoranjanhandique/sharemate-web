157711fd27da5cc332ccd5f63fa9004c967480c3
Manoranjan <manoranjanhandique@gmail.com>
2025-01-19 21:54:27 +0530

    lazy Suspense added

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,11 @@ import { createBrowserRouter } from 'react-router-dom'
 import SignIn from "./components/SignIn";
 import Home from "./components/Home";
 import SignUp from "./components/SignUp";
-import Group from "./components/Group";
+// import Group from "./components/Group";
 import Dashboard from "./components/Dashboard";
 import ProtectedRoute from "./components/ProtectedRoute";
+import { lazy, Suspense } from "react";
+const Group= lazy(()=>import("./components/Group"))
 
 const App = createBrowserRouter([
   {
@@ -27,9 +29,11 @@ const App = createBrowserRouter([
       {
         path: "/create-group",
         element: (
-          <ProtectedRoute>
-            <Group />
-          </ProtectedRoute>
+          <Suspense fallback={<div>Loading...</div>}>
+            <ProtectedRoute>
+              <Group />
+            </ProtectedRoute>
+          </Suspense>
         ),
       },
       {
